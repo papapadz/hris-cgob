a541158fb938625eedb26ff6b90f1967b328240a
Padz <benpadz08@gmail.com>
2021-02-02 17:42:47 +0800

    Create DepartmentsSeeder.php

---
+++ database/seeders/DepartmentsSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Department;
+
+class DepartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'Office of the City Mayor',
+            'Human Resource Management Office',
+            'City Budget Office',
+            'Office of the City Accountant',
+            'Office of the City Planning & Development Coordinator',
+            'Office of the Sangguniang Panlungsod',
+            'Office of the City Social Welfare & Development Officer',
+            'Office of the City Health Officer',
+            'Public Employment Service Office',
+            'Community Affairs and Development Office',
+            'Office of the City Civil Registrar',
+            'Office of the City Treasurer',
+            'Office of the City Assessor',
+            'Office of the Mayor – Market Section',
+            'Office of the City Treasurer – Market Section',
+            'Business Permits and Licenses Section',
+            'Tourism and Events Section',
+            'Disaster Risk Reduction Management Division',
+            'Office of the City Engineer',
+            'Office of the City Building Official',
+            'Office of the City General Service Officer',
+            'Office of the City Agriculturist',
+            'Office of the City Veterinarian'
+        ];
+
+        foreach($data as $k => $d) {
+            Department::create([
+                'department' => $d['office'],
+                ''
+            ]);
+        }
+    }
+}
