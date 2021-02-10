e22037b201331029f2437c08d937ce5b45c95b0d
Padz <benpadz08@gmail.com>
2021-02-11 07:33:37 +0800

    updates

--- public/js/menu-edit.js
+++ public/js/menu-edit.js
@@ -161,7 +161,7 @@ document.getElementById("type").onchange = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\hris-cgob\resources\js\coreui\menu-edit.js */"./resources/js/coreui/menu-edit.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\hris-cgob\resources\js\coreui\menu-edit.js */"./resources/js/coreui/menu-edit.js");
 
 
 /***/ })
